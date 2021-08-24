6d80b84a81e046799778885ceda680b259b9546e
motianlun99 <464716448@qq.com>
2021-08-24 18:48:14 +0800

    Update solution146.cpp

--- solution146.cpp
+++ solution146.cpp
@@ -6,7 +6,16 @@
  */
 #include<iostream>
 #include<map>
+/**
+LRU Cache具备的操作：
 
+set(key,value)：如果key在hashmap中存在，则先重置对应的value值，然后获取对应的节点cur，将cur节点从链表删除，并移动到链表的头部；若果key在hashmap不存在，
+则新建一个节点，并将节点放到链表的头部。当Cache存满的时候，将链表最后一个节点删除即可。
+get(key)：如果key在hashmap中存在，则把对应的节点放到链表头部，并返回对应的value值；如果不存在，则返回-1。
+LRU的c++实现
+LRU实现采用双向链表 + Map 来进行实现。这里采用双向链表的原因是：如果采用普通的单链表，则删除节点的时候需要从表头开始遍历查找，效率为O(n)，
+采用双向链表可以直接改变节点的前驱的指针指向进行删除达到O(1)的效率。使用Map来保存节点的key、value值便于能在O(logN)的时间查找元素,对应get操作。
+*/
 using namespace std;
 
 /**
@@ -47,7 +56,10 @@ public:
       return -1;
     }
   }
-
+  /*
+  set(key, value)操作需要分情况判断。如果当前的key值对应的节点已经存在，则将这个节点取出来，并且删除节点所处的原有的位置，
+  并在头部插入该节点；如果节点不存在节点中，这个时候需要在链表的头部插入新节点，插入新节点可能导致容量溢出，如果出现溢出的情况，则需要删除链表尾部的节点。
+  */
   void set(int key, int value)
   {
     map<int, CacheNode *>::iterator it = mp.find(key);
